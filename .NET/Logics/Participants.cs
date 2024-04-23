911fe0b9fed9e2e0a32b5e0e5b26ccb17353e53a
srilakshmi-depuri <167881775+srilakshmi-depuri@users.noreply.github.com>
2024-04-23 19:03:09 +0530

    Create Participants.cs

---
+++ .NET/Logics/Participants.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamePlanner1Logics
+{
+    public class Participants
+    {
+        
+        public string TeamName { set; get; }
+        public string ParticipantName { get; set; }
+    }
+}
